50b34813f76057b1942c7c1d5cc6ca352b47ed43
AngelPM9506 <parra_mondragon@live.com>
2022-08-13 16:17:29 -0500

    BackEnd Basico

--- api/src/controllers/TravelController.js
+++ api/src/controllers/TravelController.js
@@ -1,16 +1,17 @@
 const { Travel, Country } = require('../db');
 
 const TravelController = {
+    /**obtener todas las actividades */
     getAll: async (req, res) => {
-        let travels = await Travel.findAll({include: [Country]});
+        let travels = await Travel.findAll({ include: [Country] });
         if (travels.length === 0) {
-            return res.status(404).json({message: 'Aun no se registra ninguna actividad'})
+            return res.status(404).json({ message: 'Aun no se registra ninguna actividad' })
         }
         res.json(travels);
     },
+    /**Crear nueva actividad */
     newActivity: async (req, res) => {
         let { name, dificultad, duracion, temporada, code } = req.body;
-        console.log(req.body);
         let args = { name, dificultad, duracion, temporada };
         if (!name, !duracion, !temporada, !code) return res.status(400).json({ message: 'Faltan datos necesarios' });
         let country = await Country.findByPk(code.toUpperCase());
