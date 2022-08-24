c5f8bdf511b033c3f34c23abdc678791d2af5b7f
AngelPM9506 <parra_mondragon@live.com>
2022-08-24 12:01:51 -0500

    Mejoras en las variables de entorno 02

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ const { conn} = require('./src/db.js');
 const { findCountries } = require('./src/functios');
 
 // Syncing all the models at once.
-conn.sync({force: false}).then(async function () {
+conn.sync({force: true}).then(async function () {
   await findCountries();
   server.listen(process.env.PORT, () => {
     console.log(`%s listening at http://127.0.0.1:${process.env.PORT}`); // eslint-disable-line no-console
